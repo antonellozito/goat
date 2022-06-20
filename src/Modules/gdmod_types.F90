fc6b4ed0e5e1d089e36a3bda1d86e6244c4b76f4
Sander Van den KerkhofÃ <sander.vandenkerkhof@kuleuven.be>
2022-06-20 09:45:16 +0200

    Added standard precision types as parameters. Also, UDTs are now public by default, as these should be available to the user.

--- src/Modules/gdmod_types.F90
+++ src/Modules/gdmod_types.F90
@@ -8,7 +8,12 @@
 !============
 ! This module contains all the type definitions used in the grid 
 ! deformation module. See the option setting routines in gdmod_userinput
-! for an explanation of the different fields and options. 
+! for an explanation of the different fields and options. Additionally,
+! precision types are set here that can (should) be used in all 
+! subroutines. 
+
+! Note: default accessibility is set to public, since all these types 
+! and corresponding functions are free to be used. 
 
 module gdmod_types
 
@@ -17,20 +22,24 @@ module gdmod_types
     ! The usual
     implicit none
     save
-    private 
+    public 
 
-    ! All public types
-    public RunfileOptionsUDT
-    public ExportOptionsUDT
-    public DesignOptionsUDT
-    public GridOptionsUDT
-    public NumOptionsUDT
-    public MagneticFieldOptionsUDT
-    public GridUDT
-    public DesignParamsUDT 
+    ! Precision types
+    integer, parameter       :: R4      = selected_real_kind(6)
+    integer, parameter       :: R8      = selected_real_kind(14)
+    integer, parameter       :: R16     = selected_real_kind(33)
+
+    integer, parameter       :: C32     = 32
+    integer, parameter       :: C64     = 64
+    integer, parameter       :: C132    = 132
+
+    integer, parameter       :: I4      = selected_int_kind(4)
+    integer, parameter       :: I8      = selected_int_kind(8)
+    integer, parameter       :: I16     = selected_int_kind(16)
+
+    ! All private types
     
-    ! All public functions
-    public SetDefaultRunfileOptions
+    ! All private functions
 
     !==================================================================!
     !                                                                  !
@@ -46,25 +55,25 @@ module gdmod_types
 
     ! Options for the main runfile type
     type RunfileOptionsUDT
-        character(32)       :: runtype ! type of run: 'optimize' or 'test'
-        character(32)       :: gridtype ! type of grid: 'plasma' 
-        character(32)       :: meth ! method for grid deformation: 'KKT'
-        logical             :: export ! do export? 
+        character(C32)          :: runtype ! type of run: 'optimize' or 'test'
+        character(C32)          :: gridtype ! type of grid: 'plasma' 
+        character(C32)          :: meth ! method for grid deformation: 'KKT'
+        logical                 :: export ! do export? 
     end type
 
     ! Options for exporting
     type ExportOptionsUDT
-        character(32)       :: gridformat ! format of grid to export
+        character(C32)       :: gridformat ! format of grid to export
     end type
 
     ! Options for design variables
     type DesignVariableOptionsUDT
-        character(32)       :: type ! type of design variables
+        character(C32)       :: type ! type of design variables
     end type
 
     ! Options for the cost function
     type CostFunctionOptionsUDT
-        character(32)       :: type ! the cost function format
+        character(C32)       :: type ! the cost function format
     end type
 
     ! Options for the constraints
@@ -88,19 +97,19 @@ module gdmod_types
 
     ! Options for the grid
     type GridOptionsUDT
-        character(32)       :: inputtype
-        logical             :: vesselrefine
-        real(kind=8)        :: vesselmaxdist
+        character(C32)          :: inputtype
+        logical                 :: vesselrefine
+        real(R8)                :: vesselmaxdist
     end type  
 
     ! Options for numerics
     type NumOptionsUDT
-        integer(kind=8)     :: itmax 
+        integer(R8)     :: itmax 
     end type
 
     ! Options for magnetic field
     type MagneticFieldOptionsUDT
-        character(32)       :: readmeth
+        character(C32)       :: readmeth
     end type
 
 
@@ -109,25 +118,27 @@ module gdmod_types
     ! Vertex structure
     type VertexUDT
         ! Coordinates
-        real(kind=8), allocatable           :: x,y 
+        real(R8), allocatable               :: x(:),y(:) 
 
         ! Logicals and indices
         logical, allocatable                :: BV
-        integer(kind=8), allocatable        :: fieldlineID 
+        integer(I8), allocatable            :: fieldlineID 
+        integer(I8)                         :: ntot
+
     end type
 
     ! Face structure
     type FaceUDT
         ! Logicals and indices
-        integer(kind=8), allocatable        :: vert
+        integer(I8), allocatable            :: vert
         logical, allocatable                :: V 
     end type
 
     ! Cell structure
     type CellUDT
         ! Logicals and indices
-        integer(kind=8), allocatable        :: vert
-        integer(kind=8)                     :: ntot
+        integer(I8), allocatable            :: vert
+        integer(I8)                         :: ntot
     end type
 
     ! Main grid structure
@@ -147,7 +158,7 @@ module gdmod_types
     ! Design parameter structure
     type DesignParamsUDT
         ! Design variables
-        real, allocatable   :: phi 
+        real, allocatable   :: phi(:) 
 
     end type
 
@@ -174,6 +185,32 @@ module gdmod_types
         options%export      = .true.  
 
     end subroutine
+
+    ! Grid routines
+    !==============
+    ! Allocation
+    subroutine AllocateGrid(grid)
+        ! Description
+        !============
+        ! Allocate the fields in the vertex, faces, and cell structures
+        ! of the grid. 
+
+        ! The usual
+        implicit none
+
+        ! Declare variables
+        type(GridUDT)       :: grid
+
+        ! Return if already allocated
+        if (allocated(grid%vert%x)) return
+
+        ! Allocate vertex data
+        allocate(grid%vert%x(grid%vert%ntot))     
+
+
+    end subroutine
+
+    ! Deallocation
 
 
 end module
