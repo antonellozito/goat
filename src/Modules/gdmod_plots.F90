0948db8cd8eedc1ec247cb922f76422e84c95ec3
Sander Van den KerkhofÃ <sander.vandenkerkhof@kuleuven.be>
2022-08-08 17:13:17 +0200

    Added a routine to extract the (sorted) vertices of a single polygon. Also added a visualization routine for polygons.

--- src/Modules/gdmod_plots.F90
+++ src/Modules/gdmod_plots.F90
@@ -56,6 +56,54 @@ module gdmod_plots
     !                            Main plotters                         !
     !------------------------------------------------------------------!
 
+    ! Generic
+    !========
+    subroutine Plot2DPolygon(x,y,n,gnuplotoptions)
+
+        ! Description
+        !============
+        ! Plot a generic 2D polygon based on x and y coordinates. 
+        ! Different polygons can be separated using NaNs, for which a 
+        ! blank line will be written to disconnect the polygons in the
+        ! plot. 
+
+        ! The usual
+        implicit none
+
+        ! Declare variables
+        integer                             :: n, i, fu
+        real(R8), dimension(n)              :: x, y
+        character(*)                        :: gnuplotoptions
+
+        ! Initialize
+        !===========
+        ! Set the correct directories
+        call SetGnuplotNames(plotfile,datafile,'plot2dpolygon')
+
+        ! Write the data file
+        !====================
+        ! Write vertex coordinates to file
+        open (action='write', file=trim(datafile), newunit=fu, &
+             status='replace')
+    
+        do i = 1, n
+            if (isnan(x(i))) then 
+                ! Write blank line
+                write(fu, *)
+            else
+                ! Write coordinates
+                write (fu, *) x(i), y(i)
+            end if
+        end do
+    
+        close (fu)
+
+        ! Call plotter
+        !=============
+        call gnuplotexe(gnuplotoptions,trim(plotfile))
+
+    end subroutine
+
     ! Grid 
     !=====
     ! Grid (nodes)
