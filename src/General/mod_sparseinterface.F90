1c79e20bb39d2228de92758a3bdc4ef1061d69e0
Sander Van den KerkhofÃ <sander.vandenkerkhof@kuleuven.be>
2022-10-17 08:01:10 +0200

    Added a sparse matrix interface that provides a simple type which stores row, column, value data of a sparse matrix. For interfacing with a decent sparse algebra library in the future.

---
+++ src/General/mod_sparseinterface.F90
@@ -0,0 +1,187 @@
+!======================================================================!
+!                                                                      !
+!                               DOCUMENTATION                          !
+!                                                                      !
+!======================================================================!
+
+! Description
+!============
+! This module serves as an interface between 'our' sparse matrix 
+! representation (which only serves as a temporary fix) and any other 
+! decent sparse library. Basically, an intermediate derived type is 
+! provided that simply stores the row and column indices of the 
+! non-zero elements along with their values, and the dimensions nrow and
+! ncol of the matrix. This should be sufficient to construct the sparse
+! matrix in any desireable format. 
+
+module mod_sparseinterface
+    
+    ! Initialize
+    !============
+    ! Load modules
+    use mod_precision
+
+    ! The usual
+    implicit none
+    save
+    public 
+
+    !==================================================================!
+    !                                                                  !
+    !                               TYPES                              !
+    !                                                                  !
+    !==================================================================!
+
+    ! Abstract types
+    !===============
+    ! Sparse matrix type
+    type :: MySparseUDT
+
+        ! Description
+        !============
+        ! Our own sparse matrix format. 
+
+        ! Fields:
+        ! - nrow:           number of rows of the matrix 
+        ! - ncol:           number of columns of the matrix 
+        ! - nval:           number of non-zero values of the matrix 
+        ! - row:            row indices (nval-by-1)
+        ! - col:            column indices (nval-by-1)
+        ! - val:            values (nval-by-1)
+
+        integer(I8)                 :: nrow = 0, ncol = 0, nval = 0
+        integer(I8), allocatable    :: row(:), col(:)
+        real(R8), allocatable       :: val(:)
+
+    contains
+
+        ! Constructor
+        procedure :: Initialize     => InitializeMySparse
+
+        ! Housekeeping procedures
+        procedure :: Allocate       => AllocateMySparse
+        procedure :: Deallocate     => DeallocateMySparse
+
+        ! Destructor
+        final :: DestroyMySparse
+
+    end type
+
+    !==================================================================!
+    !                                                                  !
+    !                            INTERFACES                            !
+    !                                                                  !
+    !==================================================================!
+
+    contains
+
+    !==================================================================!
+    !                                                                  !
+    !                               ROUTINES                           !
+    !                                                                  !
+    !==================================================================!
+
+    ! Constructor
+    subroutine InitializeMySparse(mysparse, nrow, ncol, nval)
+
+        ! Description
+        !============
+        ! Sparse matrix constructor, including allocation
+
+        ! Initialize
+        !===========
+        ! The usual
+        implicit none
+
+        ! Declare variables
+        !==================
+        ! Arguments
+        class(MySparseUDT)           :: mysparse 
+        integer(I8), intent(in)     :: nrow, ncol, nval 
+
+        ! Construct
+        !==========
+        ! Set dimensions
+        mysparse%nrow = nrow 
+        mysparse%ncol = ncol
+        mysparse%nval = nval
+
+        ! Allocate
+        call mysparse%Allocate()
+
+    end subroutine
+
+    ! Allocation
+    subroutine AllocateMySparse(mysparse)
+
+        ! Description
+        !============
+        ! Allocate. It is assumed that nrow, ncol, nval are set to the
+        ! correct values. 
+
+        ! Initialize
+        !===========
+        ! The usual
+        implicit none
+
+        ! Declare variables
+        !==================
+        ! Arguments
+        class(MySparseUDT)       :: mysparse
+
+        ! Allocate
+        !=========
+        allocate(mysparse%col(mysparse%nval))
+        allocate(mysparse%row(mysparse%nval))
+        allocate(mysparse%val(mysparse%nval))
+
+    end subroutine
+
+    ! Deallocation
+    subroutine DeallocateMySparse(mysparse)
+
+        ! Description
+        !============
+        ! Deallocate.
+
+        ! Initialize
+        !===========
+        ! The usual
+        implicit none
+
+        ! Declare variables
+        !==================
+        ! Arguments
+        class(MySparseUDT)       :: mysparse
+
+        ! Allocate
+        !=========
+        deallocate(mysparse%col)
+        deallocate(mysparse%row)
+        deallocate(mysparse%val)
+
+    end subroutine
+
+    ! Destructor
+    subroutine DestroyMySparse(mysparse)
+
+        ! Description
+        !============
+        ! Destructor, to be called when the object should be destroyed
+
+        ! Initialize
+        !===========
+        ! The usual
+        implicit none
+
+        ! Declare variables
+        !==================
+        type(MySparseUDT)       :: mysparse
+
+        ! Destroy
+        !========
+        call mysparse%Deallocate()
+
+    end subroutine
+
+end module
