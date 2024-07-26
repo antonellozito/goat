5d4a09fcfe37daac5b75fd145d9432820d21e3e3
Sander <sander.vandenkerkhof@kuleuven.be>
2024-07-26 11:16:28 +0200

    Output is now always written in ./output, similar to solps

--- src/General/mod_plotter.F90
+++ src/General/mod_plotter.F90
@@ -39,11 +39,7 @@ module mod_plotter
     public 
 
     ! Define the (relative) file directory where the plot are located
-#ifdef SOLPS
-    character(*), parameter     :: plotdir = '../output'
-#else
     character(*), parameter     :: plotdir = './output'
-#endif
 
     ! Define the (general) plot filename and data filename
     character(:), allocatable                 :: plotfile
