e97e53a7d81c2878e0e6433a986a653ac9bd2ca3
Sander <sander.vandenkerkhof@kuleuven.be>
2024-10-23 06:35:19 +0200

    Bugfix: fixed vessel polygon not being properly built in some cases when excluding vessel segments

--- src/IO/Input/ExtractVesselData.F90
+++ src/IO/Input/ExtractVesselData.F90
@@ -117,7 +117,7 @@ subroutine ExtractVesselData(vessel, vesseloptions)
             vID = vID + vs(i)%np 
 
             ! Add NaN
-            if (i .ne. (nvs - nexcl)) then 
+            if (cc .ne. (size(tempx))) then 
                 tempx(cc+1) = NaN 
                 tempy(cc+1) = NaN 
                 templabels(cc+1, :) = 0
