d81fed84437cc7623e7dbc683cccd5b8aec89fa0
Sander <sander.vandenkerkhof@kuleuven.be>
2025-06-04 11:08:38 +0200

    Added several operations for structure deformer and updated script

--- scripts/StructureDeformerScript.py
+++ scripts/StructureDeformerScript.py
@@ -11,9 +11,10 @@ import copy
 import os 
 
 # Load structure
-structuredir = './goatf/Examples/JT60SA/Baseline/structure.dat'
-writedir  = './goatf/Examples/JT60SA/5degreesRP'
-topomeshdir = './goatf/Examples/JT60SA/Baseline/output/topomesh.dat'
+root = '/mnt/c/Users/u0110555/Desktop/code_werk/goatf'
+structuredir = root + '/goatf/Runs/JT60SA/Giulio/structure.dat'
+writedir  = root + '/goatf/Runs/JT60SA/10degreesBot'
+topomeshdir = root + '/goatf/Runs/JT60SA/Giulio/output/topomesh.dat'
 structure = dh.ReadStructureFile(structuredir)
 newstructure = copy.deepcopy(structure)
 topomesh = dh.ReadTopomeshFile(topomeshdir)
@@ -25,9 +26,13 @@ pl.PlotStructure(structure, 0)
 # 2: translate point 
 # 3: rotate entire vessel
 # 4: translate entire vessel
+# 5: add point before or after point  
+# 6: set point coordinates equal to intersection of two lines between two structure edges
+# 7: translate along edge vector
+# 8: set point coordinates equal to other point coordinates
 
 # Operations to apply translations and rotations
-ApplyDeformations = 2
+ApplyDeformations = 3
 if ApplyDeformations == 1:
     # 6cm shift of entire vessel, then rotation
     operations = [4, 1, 1]
@@ -54,8 +59,28 @@ elif ApplyDeformations == 2:
     #zdata = [0.12, 10, 10]
     zdata = [0.05, 5, 5]
 
+elif ApplyDeformations == 3:
+    # Rotate at bottom point, find new point on baffle as intersection, fix other point at intersection coordinates, translate point 2 to be below point 1 again
+    operations = [1, 1, 1, 6, 8, 8, 7, 7, 8]
+    structIDs = [10, 10, 10, 10, 21, 21, 21, 10, 14]
+    points = [3, 4, 5, 5, 1, 2, 1, 5, 1]
+
+    auxstructID1 = [0, 0, 0, 10, 10, 10, 10, 10, 21]
+    auxstructID2 = [0, 0, 0, 14, 0, 0, 0, 0, 0]
+    auxpoint1 = [0, 0, 0, 2, 5, 5, 4, 4, 2]
+    auxpoint2 = [0, 0, 0, 1, 0, 0, 0, 0, 0]
+
+    xdata = [structure[9].x[1], structure[9].x[1], structure[9].x[1], 0, 
+        0, 0, 0, 0, 0]
+    ydata = [structure[9].y[1], structure[9].y[1], structure[9].y[1], 0, 
+        0, 0, 0, 0, 0]
+    tempdist = np.sqrt((structure[20].x[0] - structure[20].x[1])**2 + (structure[20].y[0] - structure[20].y[1])**2)
+    zdata = [10, 10, 10, 0, 0, 0, -tempdist, -tempdist, 0]
+
+
+
 # Operations to adjust original structure
-else:
+elif ApplyDeformations == 0:
     operations = [2, 2, 2, 2, 2, 2, 2]
     structIDs = [7, 7, 7, 7, 7, 7, 7]
     points = [25, 26, 27, 28, 29, 30, 31]
@@ -63,6 +88,9 @@ else:
     xdata = [-0.5, -0.5, -0.5, -0.5, -0.5, -0.5, 1.0]
     ydata = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0]
     zdata = [0.07, 0.07, 0.07, 0.07, 0.12, 0.03, 0.05]
+else:
+    # Dry run, do nothing
+    operations = []
 
 
 
@@ -73,6 +101,10 @@ for i in np.arange(0, len(operations), 1):
     tx = xdata[i]
     ty = ydata[i]
     tz = zdata[i]
+    ts1 = auxstructID1[i]-1 
+    ts2 = auxstructID2[i]-1 
+    pID1 = auxpoint1[i]-1 
+    pID2 = auxpoint2[i]-1
     if operations[i] == 1:
         newx, newy = sd.RotatePoint(newstructure[ts].x[tp], newstructure[ts].y[tp], 
             tx, ty, tz)
@@ -93,13 +125,49 @@ for i in np.arange(0, len(operations), 1):
             newx, newy = sd.TranslatePoint(i.x, i.y, tx, ty, tz)
             i.x = newx 
             i.y = newy
+    elif operations[i] == 5: 
+        newx, newy = sd.AddPoint(newstructure[ts].x, newstructure[ts].y, 
+            tx, ty, tz, tp)
+        newstructure[ts].x = newx 
+        newstructure[ts].y = newy
+        if newstructure[ts].n > 0:
+            newstructure[ts].n = newstructure[ts].n + 1
+        else:
+            newstructure[ts].n = newstructure[ts].n - 1
+    elif operations[i] == 6:
+        # Intersection between two edges forms new point
+        x11 = newstructure[ts1].x[pID1]
+        x12 = newstructure[ts1].x[pID1+1]
+        y11 = newstructure[ts1].y[pID1]
+        y12 = newstructure[ts1].y[pID1+1]
+        x21 = newstructure[ts2].x[pID2]
+        x22 = newstructure[ts2].x[pID2+1]
+        y21 = newstructure[ts2].y[pID2]
+        y22 = newstructure[ts2].y[pID2+1]
+        newx, newy = sd.LineIntersections(x11, y11, x12, y12, x21, y21, x22, y22)
+        newstructure[ts].x[tp] = newx 
+        newstructure[ts].y[tp] = newy
+    elif operations[i] == 7:
+        # Translate along vector
+        tx = newstructure[ts1].x[pID1+1] -  newstructure[ts1].x[pID1]
+        ty = newstructure[ts1].y[pID1+1] -  newstructure[ts1].y[pID1]
+        newx, newy = sd.TranslatePoint(newstructure[ts].x[tp], newstructure[ts].y[tp], 
+            tx, ty, tz)
+        newstructure[ts].x[tp] = newx 
+        newstructure[ts].y[tp] = newy  
+    elif operations[i] == 8:
+        # Set vertex coordinates equal
+        newstructure[ts].x[tp] = newstructure[ts1].x[pID1]
+        newstructure[ts].y[tp] = newstructure[ts1].y[pID1]
+
+
     
 
 
 # Visualize structure, with vertex/structure IDs 
 pl.PlotStructure(structure, 1, color='r')
 pl.PlotStructure(newstructure, 1, color='g')
-pl.PlotTopologicalMesh(topomesh, 1)
+#pl.PlotTopologicalMesh(topomesh, 1)
 
 # Write output
 try:
--- scripts/python_source/StructureDeformer.py
+++ scripts/python_source/StructureDeformer.py
@@ -38,3 +38,174 @@ def TranslatePoint(px, py, dx, dy, length):
     newpy = py + dy*length 
 
     return newpx, newpy
+
+# Add a new point in an existing polygon
+def AddPoint(px, py, npx, npy, addbefore, pointindex):
+    # Description
+    #------------
+    # Add a point with coordinates npx, npy in the existing polygon 
+    # defined by the points px, py before (addbefore >= 0) or after 
+    # (addbefore < 0) the point defined by pointindex 
+
+    # Checks
+    if len(px) != len(py):
+        raise ValueError("AddPoint: px and py should have equal sizes")
+    if (pointindex < 0) or (pointindex >= len(px)):
+        raise ValueError("AddPoint: pointindex out of range")
+    
+    # Add point 
+    if addbefore < 0:
+        # Add after pointindex 
+        px = np.insert(px, pointindex+1, npx)
+        py = np.insert(py, pointindex+1, npy)
+    else:
+        px = np.insert(px, pointindex, npx)
+        py = np.insert(py, pointindex, npy)
+
+    # Return
+    return px, py
+    
+# Reset the coordinates of a point to be on the intersection of two lines
+# defined by their tangential vectors (not normalized)
+def LineIntersections(x11, y11, x12, y12, x21, y21, x22, y22):
+    disttol = 1e-12
+    macheps = 1e-12
+    # Compute
+    dx1 = x12 - x11 
+    dy1 = y12 - y11 
+    dx2 = x22 - x21 
+    dy2 = y22 - y21 
+
+    # Some checks
+    if ( (abs(dx1) < disttol) and (abs(dy1) < disttol) ): 
+        # Print warning
+        print('LineIntersections: ' +
+                'first line is a point up to distance precision, ' +
+                'results may be inaccurate. Consider rescaling the' +
+                ' coordinates')
+        
+    if ( (abs(dx2) < disttol) and (abs(dy2) < disttol) ): 
+        # Print warning
+        print('LineIntersections: ' +
+                'second line is a point up to distance precision, ' +
+                'results may be inaccurate. Consider rescaling the' +
+                ' coordinates')
+        
+
+    # Check determinant
+    det = -dy1*dx2 + dx1*dy2
+    if ( (abs(det) < macheps) ): 
+        # Parallel or collinear lines - need to check collinearity
+        
+        # Compute normal
+        nx = -(y11 - y12)
+        ny = (x11 - x12)
+        nn = np.sqrt(nx**2 + ny**2)
+
+        # Compute vector between lines
+        vx = (x11 - x21)
+        vy = (y11 - y21)
+
+        # Compute the distance
+        dist = abs( vx*nx/nn + vy*ny/nn )
+
+        # Check 
+        if (dist < disttol): 
+            # collinear lines, return inf
+            x = np.NaN
+            y = x
+            return x, y  
+        else:
+            # Parallel lines, return nan
+            x = np.NaN
+            y = x 
+            
+            return x, y 
+        
+
+    # Compute intersection
+    #=====================
+    # Hedge for small dx when computing slope 
+    if (abs(dx1) > abs(dx2)): 
+        if (abs(dx1) > disttol): 
+            r1 = dy1/dx1 
+            if (abs(dx2) > disttol): 
+                # Two non-parallel, non-vertical and non-horizontal lines
+                r2 = dy2/dx2 
+                x = (r1*x11 - r2*x21 -y11 + y21)/(r1 - r2)
+                y = r1*(x - x11) + y11
+            else:
+                # Second line is vertical line, first one is non-vertical
+                x = x21
+                y = r1*(x - x11) + y11
+                
+
+        else:
+            # Both lines are parallel - should've been captured before actually
+            # Compute normal
+            nx = -(y11 - y12)
+            ny = (x11 - x12)
+            nn = np.sqrt(nx**2 + ny**2)
+
+            # Compute vector between lines
+            vx = (x11 - x21)
+            vy = (y11 - y21)
+
+            # Compute the distance
+            dist = abs( vx*nx/nn + vy*ny/nn )
+
+            # Check 
+            if (dist < disttol): 
+                # collinear lines, return inf
+                x = np.NaN
+                y = x 
+                return x, y 
+            else:
+                # Parallel lines, return nan
+                x = np.NaN
+                y = x 
+                
+                return x, y
+            
+    else:
+        if (abs(dx2) > disttol): 
+            r2 = dy2/dx2
+            if (abs(dx1) > disttol): 
+                # Two non-parallel, non-vertical and non-horizontal lines
+                r1 = dy1/dx1 
+                x = (r1*x11 - r2*x21 -y11 + y21)/(r1 - r2)
+                y = r2*(x - x21) + y21
+            else:
+                # First line is vertical line, second one is non-vertical
+                x = x11
+                y = r2*(x - x21) + y21
+                
+
+        else:
+            # Both lines are parallel - should've been captured before actually
+            # Compute normal
+            nx = -(y11 - y12)
+            ny = (x11 - x12)
+            nn = np.sqrt(nx**2 + ny**2)
+
+            # Compute vector between lines
+            vx = (x11 - x21)
+            vy = (y11 - y21)
+
+            # Compute the distance
+            dist = abs( vx*nx/nn + vy*ny/nn )
+
+            # Check 
+            if (dist < disttol): 
+                # collinear lines, return inf
+                x = np.NaN
+                y = x 
+            else:
+                # Parallel lines, return nan
+                x = np.NaN
+                y = x 
+                
+    return x, y
+            
+        
+
