f2b5fa1ce8096b3654f591b08a42c5d54b335d8e
Nathan Vervloesem <nathan.vervloesem@kuleuven.be>
2025-08-18 18:48:47 +0200

    Start implementation of GAgridudt

--- src/Modules/Goat/goatmod_types.F90
+++ src/Modules/Goat/goatmod_types.F90
@@ -24,6 +24,7 @@ module goatmod_types
     use mod_polygon
     use mod_inputfileparser
     use mod_plotter
+    use mod_dynamicarrays
     use Interpolant
     use PolygonShapeFunction
     use PolygonLevelsetFunction2D
@@ -106,6 +107,63 @@ module goatmod_types
 
     end type
 
+    type GAVertexUDT
+
+        ! Description
+        !============
+        ! Fields:
+        ! - x, y            : coordinates [m]
+        ! - BV              : logical index to indicate if vertex lies
+        !                   on boundary
+        ! - fieldlineID     : ID of the field line the vertex lies on
+        ! - ntot            : total number of vertices  
+        ! - faceP           : ntot-by-2 array containing in the first 
+        !                   column the starting index and in the second
+        !                   column the number of faces of the cell 
+        !                   (for querying the faces of the cell
+        !                   stored in vert%facelist)            
+        ! - facelist        : list of vertex faces, to be queried as 
+        !                   vert%facelist(vert%faceP(i,1):
+        !                   vert%faceP(i,1)+vert%faceP(i,2)-1) 
+        ! - nfacelist       : length of facelist   
+        ! - cellP           : similar to faceP, but for vertex cells
+        ! - celllist        : similar to facelist, but for vertex cells
+        ! - ncellist        : length of celllist    
+        ! - neiglist        : list of neighbouring vertices (nneiglist-
+        !                   by-1)
+        ! - nneiglist       : dimension of neiglist (scalar)
+        ! - neigP           : ntot-by-2 array, analogous to faceP and 
+        !                   cellP, but for neiglist.
+        ! - psi             : psi values at vertex locations    
+        ! - bx, by          : magnetic field vector at vertex locations  
+        ! - ffbz            : ???
+
+        ! Coordinates
+        type(RealDynamicArrayUDT), allocatable        :: x(:), y(:)
+
+        ! Logicals and indices
+        logical, allocatable                          :: BV(:)
+        type(RealDynamicArrayUDT), allocatable        :: fieldlineID(:) 
+        integer(I8)                                   :: ntot = 0
+
+        type(IntegerDynamicArrayUDT), allocatable     :: faceP(:,:)
+        type(IntegerDynamicArrayUDT), allocatable     :: face(:)
+        integer(I8)                                   :: nface = 0
+
+        type(IntegerDynamicArrayUDT), allocatable     :: cellP(:,:)
+        type(IntegerDynamicArrayUDT), allocatable     :: cell(:)
+        integer(I8)                                   :: ncell = 0
+
+        type(IntegerDynamicArrayUDT), allocatable     :: neigP(:,:)
+        type(IntegerDynamicArrayUDT), allocatable     :: neig(:)
+        integer(I8)                                   :: nneig = 0
+
+        ! Other data
+        type(RealDynamicArrayUDT), allocatable, dimension(:) :: bx, &
+            by, psi, ffbz 
+
+    end type 
+
     ! Face structure
     type FaceUDT
 
@@ -137,6 +195,38 @@ module goatmod_types
         integer(I8)                         :: ntot = 0
         logical, allocatable                :: BF(:)
 
+    end type
+
+    type GAFaceUDT
+        ! Description
+        !============
+        ! Fields:
+        ! - ntot            : total number of faces
+        ! - vert            : set of (two) vertices belonging to that 
+        !                   face
+        ! - cell            : list of cell neighbours (max 2)
+        ! - cellP           : pointer for list of cell neighbours
+        ! - neig            : cell neighbours of face
+        ! - BF              : logical index that is true if the face
+        !                   is a boundary face
+        ! - label           : face labels
+        ! - TMfacelabel     : face label corresponding to topomesh ID (only when GG is used)
+        ! - reg             : face regions
+        ! - aligned         : integer that is 1 if the face is aligned
+
+
+        ! Logicals and indices
+        type(IntegerDynamicArrayUDT), allocatable         :: vert(:,:)
+
+        type(IntegerDynamicArrayUDT), allocatable         :: cell(:), &
+            label(:), reg(:), aligned(:), TMfacelabel(:)
+        !logical :: aligned(:)
+        type(IntegerDynamicArrayUDT), allocatable         :: cellP(:,:)
+        integer(I8)                                       :: ncell = 0
+
+        integer(I8)                                       :: ntot = 0
+        type(IntegerDynamicArrayUDT), allocatable         :: BF(:)
+        !logical, allocatable                                 :: BF(:)
     end type
 
     ! Cell structure
@@ -178,6 +268,48 @@ module goatmod_types
         real(R8), allocatable, dimension(:) :: psi, bp, bt, x, y
         integer(I8), allocatable, dimension(:)  :: cflags, reg, &
             ft
+    end type
+
+    type GACellUDT
+
+        ! Description
+        !============
+        ! Fields:   
+        ! - ntot            : total number of cells
+        ! - vertP           : ntot-by-2 array containing in the first 
+        !                   column the starting index and in the second
+        !                   column the number of vertices of the cell 
+        !                   (for querying the vertices of the cell
+        !                   stored in cells%vert)            
+        ! - vert            : list of cell vertices, to be queried as 
+        !                   cells%vert(cells%vertP(i,1):
+        !                   cells%vertP(i,1)+cells%vertP(i,2)-1) 
+        ! - nvert          : length of vert
+        ! - faceP           : similar to vertP, but for faces
+        ! - face            : similar to vert, but for faces
+        ! - nface          : similar to nvert, but for faces
+        ! - GC              : ntot-by-1 logical vector indicating if the
+        !                   cell is a guard cell
+
+
+        ! Logicals and indices
+        type(IntegerDynamicArrayUDT), allocatable        :: vertP(:,:)
+        type(IntegerDynamicArrayUDT), allocatable        :: vert(:)
+        integer(I8)                                      :: nvert = 0
+
+        type(IntegerDynamicArrayUDT), allocatable        :: faceP(:,:)
+        type(IntegerDynamicArrayUDT), allocatable        :: face(:)
+        integer(I8)                                      :: nface = 0
+
+        logical, allocatable                             :: GC(:)
+
+        integer(I8)                                      :: ntot = 0, ngc
+        
+        type(RealDynamicArrayUDT), allocatable, dimension(:) :: psi, &
+            bp, bt, x, y
+        integer(I8), allocatable, dimension(:)  :: cflags, reg, &
+            ft
+
     end type
 
     ! Boundary structure
@@ -299,6 +431,71 @@ module goatmod_types
 
     end type
 
+    type GAFluxDataUDT
+
+        ! Description
+        !============
+        ! This data type contains all information regarding flux tubes 
+        ! and flux surfaces, and the cells associated with these. 
+        ! Fields:
+        !
+        ! - nFt:            : total number of flux tubes (scalar, to be 
+        !                   read in)
+        ! - nFs:            : total number of flux surfaces (scalar, to be
+        !                   read in)
+        ! - fluxtubecellsP  : nFt-by-2 array where the first index 
+        !                   is the start index in the fluxtubecells 
+        !                   array, and the second the amount of cells of
+        !                   the flux tube.
+        ! - fluxtubecells   : nCv(number of cells)-by-1 array containing 
+        !                   the cell numbers that correspond to flux 
+        !                   tubes. 
+        ! - fluxtubefacesP  : nFt-by-2 array where the first index 
+        !                   is the start index in the fluxtubefaces 
+        !                   array, and the second the amount of faces of
+        !                   the flux tube.
+        ! - fluxtubefsIDs   : flux surface IDs that bound the flux tube
+        !                   (nFt-by-2)
+        ! - fluxtubefaces   : nFv(number of faces)-by-1 array containing 
+        !                   the face numbers that correspond to flux 
+        !                   tubes. 
+        ! - fluxsurfacefacesP  : nFs-by-2 array where the first index 
+        !                   is the start index in the fluxsurfacefaces
+        !                   array, and the second the amount of faces of
+        !                   the flux surface.
+        ! - fluxsurfacefaces   : nFv(number of faces)-by-1 array containing 
+        !                   the face numbers that correspond to flux 
+        !                   surfaces. 
+        ! - fluxsurfaceID   : nv(number of vertices)-by-1 array 
+        !                   containing the flux tube ID of each vertex
+
+        ! Logicals and indices
+        integer(I8)                         :: nFt = 0
+        integer(I8)                         :: nFs = 0
+
+        ! Arrays, flux tube data
+        type(IntegerDynamicArrayUDT), allocatable :: &
+            fluxtubecellsP(:,:)
+        type(IntegerDynamicArrayUDT), allocatable :: fluxtubecells(:)
+        type(IntegerDynamicArrayUDT), allocatable :: &
+            fluxtubefacesP(:,:)
+        type(IntegerDynamicArrayUDT), allocatable :: fluxtubefaces(:)
+        type(IntegerDynamicArrayUDT), allocatable :: &
+            fluxtubefsIDs(:, :), fluxtuberegID(:)
+        logical, allocatable                :: isclosedft(:)
+
+        ! Arrays, flux surface data
+        type(IntegerDynamicArrayUDT), allocatable :: &
+            fluxsurfacefacesP(:,:)
+        type(IntegerDynamicArrayUDT), allocatable :: &
+            fluxsurfacefaces(:)
+        type(IntegerDynamicArrayUDT), allocatable :: fluxsurfaceID(:)
+        type(IntegerDynamicArrayUDT), allocatable :: &
+            fluxsurfaceneig(:), fluxsurfaceneigP(:, :)
+        type(RealDynamicArrayUDT), allocatable    :: fluxsurfacepsi(:)
+
+    end type
+
     ! Structured grid data (to be removed in the future)
     type StructuredGridDataUDT
 
@@ -370,7 +567,7 @@ module goatmod_types
         ! - divFcP              : pointer for the list of divertor faces
 
         ! Flux data
-        type(FluxDataUDT)           :: fluxdata
+        type(GAFluxDataUDT)           :: fluxdata
 
         ! Legacy data of structured grid
         type(StructuredGridDataUDT) :: sglegacy
@@ -394,6 +591,77 @@ module goatmod_types
         integer(I8)                             :: nxp, nsp, nop, ntp, &
             ndiv, ndivFc
 
+    end type
+
+    type GAGridDataUDT
+
+        ! Description
+        !===========
+        ! Data type to collect all other grid related data which is not 
+        ! linked to the core data, i.e. the cells, vertices, and faces.
+        ! This can be additional info from the grid generator on flux 
+        ! tubes, for example, and interconnection data thereof (e.g. 
+        ! the cells that belong to a certain flux tube). 
+        ! Fields:
+        !
+        ! - fluxdata            : UDT with all flux data such as flux
+        !                       flux tube data, flux surfaces, ... 
+        ! - sglegacy            : data from legacy structured grids
+        ! - OMPcell, OMPface    : cells and faces belonging to outer mid
+        !                       plane
+        ! - IMPcell, IMPface    : same, but inner mid plane
+        ! - OMPr, OMPz          : points defining line segment of OMP
+        ! - IMPr, IMPz          : same but for inner mid plane
+        ! - topoflag            : flag indicating the topological mesh 
+        !                       type (see also mod_definitions)
+        ! - xpointID            : array containing all X-point vertex IDs
+        ! - isprimaryxp         : integer array which is 1 if the 
+        !                       x-point is a primary x-point (i.e. it 
+        !                       lies on a separatrix that is connected
+        !                       directly to a core region), otherwise 0
+        ! - nxp                 : number of x-points
+        ! - spointID            : array containing all strike point vertex IDs
+        ! - nsp                 : number of strike points
+        ! - opointID            : O point IDs (in the grid)
+        ! - nop                 : number of o points
+        ! - tpointID            : Tangency point IDs (but here tangency 
+        !                       points are only those tangency points 
+        !                       that have a closed contour in the grid!)
+        ! - ntp                 : number of t points
+        ! - spointxpID          : x-point vertex number of the x-point 
+        !                       the strike point belongs to
+        ! - ndiv                : number of divertor targets/plates
+        ! - spointdivID         : list of targets that belong to strike 
+        !                       points (one target each)
+        ! - tpointdivID         : same as above but for tangency points
+        ! - ndivFc              : total number of divertor faces
+        ! - divFc               : list of divertor faces
+        ! - divFcP              : pointer for the list of divertor faces
+
+        ! Flux data
+        type(GAFluxDataUDT)           :: fluxdata
+
+        ! Legacy data of structured grid
+        type(StructuredGridDataUDT) :: sglegacy
+
+        ! Topological mesh type
+        integer(I8)                             :: topoflag
+        
+        ! OMP & IMP
+        integer(I8), allocatable, dimension(:)  :: OMPcell, OMPface, &
+            IMPcell, IMPface
+        integer(I8)                             :: nOMPcell, nOMPface, &
+            nIMPcell, nIMPface
+        real(R8), dimension(1:2)                :: OMPr, OMPz, IMPr, &
+            IMPz
+
+        ! X-point(s), strike points, o points, ...
+        integer(I8), allocatable, dimension(:)  :: xpointID, &
+            spointID, opointID, tpointID, isprimaryxp, spointxpID, &
+            divFc, spointdivID, tpointdivID
+        integer(I8), allocatable, dimension(:, :)   :: divFcP
+        integer(I8)                             :: nxp, nsp, nop, ntp, &
+            ndiv, ndivFc    
     end type
 
     ! Main grid structure
@@ -429,6 +697,37 @@ module goatmod_types
         type(BndUDT), allocatable           :: bnd(:)
 
     end type
+
+    type GAGridUDT
+        ! Description
+        !============
+        ! Data structure containing all the grid data substructures for grid adaptation. 
+        ! Fields:
+        !
+        ! - vert            : see type definition for description
+        ! - face           
+        ! - cell
+        ! - data
+        ! - bnd
+        !
+        ! Note: the bnd substructure has to be allocated separately
+
+        ! Vertices
+        type(GAVertexUDT)                     :: vert
+
+        ! Faces
+        type(GAFaceUDT)                       :: face
+
+        ! Cells
+        type(GACellUDT)                       :: cell
+
+        ! Additional data
+        type(GAGridDataUDT)                   :: data
+
+        ! Boundaries
+        type(BndUDT), allocatable           :: bnd(:)
+    end type
+
 
     !------------------------------------------------------------------!
     !                               Numerics                           !
